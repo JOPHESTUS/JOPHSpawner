27de13d7408085bb13e7d376f2305e3b85bd029b
JOPHESTUS <jophestus@jophest.us>
2013-06-16 14:14:06 +1000

    Added dragon egg pickup feature

--- src/me/jophestus/JOPHSpawner/JOPHSpawner.java
+++ src/me/jophestus/JOPHSpawner/JOPHSpawner.java
@@ -11,8 +11,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import org.bukkit.material.MaterialData;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JOPHSpawner extends JavaPlugin implements Listener {
@@ -28,6 +30,7 @@ public class JOPHSpawner extends JavaPlugin implements Listener {
 		if (event.getBlock().getType() == Material.MOB_SPAWNER) {
 			Block block = event.getBlock();
 			Player player = event.getPlayer();
+			
 
 			if (player.hasPermission("JOPHSpawner.use")) {
 				block.setType(Material.AIR);
@@ -41,6 +44,31 @@ public class JOPHSpawner extends JavaPlugin implements Listener {
 				player.sendMessage(ChatColor.GREEN + "[JOPHSpawner]"
 						+ ChatColor.YELLOW
 						+ " You can't break and pick up spawners :(");
+
+			}
+		}
+		
+	
+	}
+	@EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+	public void onClick(PlayerInteractEvent event){
+		if (event.getClickedBlock().getType() == Material.DRAGON_EGG) {
+			Block block = event.getClickedBlock();
+			Player player = event.getPlayer();
+			
+
+			if (player.hasPermission("JOPHSpawner.dragonegg")) {
+				block.setType(Material.AIR);
+
+				block.getWorld().dropItemNaturally(block.getLocation(),
+						new ItemStack(Material.DRAGON_EGG, 1));
+				player.sendMessage(ChatColor.GREEN + "[JOPHSpawner] "
+						+ ChatColor.YELLOW + "Dragon Egg Dropped :)");
+				event.setCancelled(true);
+			} else {
+				player.sendMessage(ChatColor.GREEN + "[JOPHSpawner]"
+						+ ChatColor.YELLOW
+						+ " You can't break and pick up Dragon Eggs :(");
 
 			}
 		}
